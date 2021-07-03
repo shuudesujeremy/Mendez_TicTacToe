0dc5fa7b5ea20e73da06ea47156eb4b20b701d97
Jeremy Mendez <iamjeremymendez234@gmail.com>
2021-07-03 13:51:56 +0800

    form design

--- Mendez_TicTacToe/Form1.Designer.cs
+++ Mendez_TicTacToe/Form1.Designer.cs
@@ -159,7 +159,7 @@ namespace Mendez_TicTacToe
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Location = new System.Drawing.Point(34, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(401, 55);
             this.label1.TabIndex = 11;
@@ -189,7 +189,7 @@ namespace Mendez_TicTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 710);
+            this.ClientSize = new System.Drawing.Size(466, 572);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
